24750798e162f7eade3bc61eaaaf7d282b1a2395
Pablo Vicente <pvicente@gmail.com>
2016-10-28 09:26:02 -0300

    Added file watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
+'use strict';
 const gulp = require('gulp');
 const eslint = require('gulp-eslint');
+const nodemon = require('gulp-nodemon');
 
 gulp.task('lint', () => {
   // All files but ignore node_modules
@@ -9,6 +11,26 @@ gulp.task('lint', () => {
     .pipe(eslint.failAfterError());
 });
 
-gulp.task('default', ['lint'], function () {
-  // This will only run if the lint task is successful...
+
+gulp.task('develop', function () {
+  const stream = nodemon({script: 'server.js',
+    ext: 'html js',
+    ignore: ['ignored.js'],
+    tasks: ['lint']});
+
+  stream
+    .on('restart', function () {
+      console.log('restarted!');
+    })
+    .on('crash', function() {
+      console.error('Application has crashed!\n');
+      stream.emit('restart', 5);  // restart the server in 10 seconds
+    });
 });
+
+gulp.task('default', ['lint', 'develop'], function () {
+
+});
+
+
+
