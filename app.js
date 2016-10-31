a681e865cf4f6e4c2ac23d87299e1410a89d7824
Pablo Vicente <pvicente@gmail.com>
2016-10-31 20:49:01 -0300

    Fixed BodyParser and ServerAcceptable configurations

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const restify = require('restify');
 const config  = require('config');
 const routes  = require('./routes');
+const os = require('os');
 
 
 exports.createServer = createServer;
@@ -28,7 +29,37 @@ function createServer (logger) {
   let server = restify.createServer(settings);
 
   server.use(restify.acceptParser(server.acceptable));
+  server.use(restify.bodyParser({
+    maxBodySize: 6000000,
+    mapParams: true,
+    mapFiles: false,
+    overrideParams: false,
+    keepExtensions: false,
+    uploadDir: os.tmpdir(),
+    multiples: true,
+    hash: 'sha1'
+  }));
   server.use(restify.queryParser());
+  server.use(restify.CORS({
+    origins: ['*'],   // Set your permited origins
+    credentials: false                 // defaults to false
+    // headers: ['x-foo'] any headers you might need
+  }));
+  server.use(restify.dateParser(5)); // Is this useful if not using some type of expiration tokens?
+  server.use(restify.gzipResponse()); // Yes accept compressed content!
+  server.use(restify.throttle({ // @TODO Something Redis based will work better probably
+    burst: 100,
+    rate: 50,
+    ip: true,
+    overrides: {
+      '192.168.1.1': {
+        rate: 0,        // unlimited
+        burst: 0
+      }
+    }
+  }));
+
+
 
   server.on('NotFound', function (req, res, next) {
     if (logger) {
